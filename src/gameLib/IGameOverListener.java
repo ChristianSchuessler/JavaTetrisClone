445e009d32fd5e20252176368bfb35145d37d6ea
christian.schuessler@freenet.de <christian.schuessler@freenet.de>
2017-05-02 22:22:41 +0200

    implemented score

---
+++ src/gameLib/IGameOverListener.java
@@ -0,0 +1,10 @@
+package gameLib;
+
+public interface IGameOverListener
+{
+	/**
+	 * informs the listener that the game is over because
+	 * a brick touched the top line
+	 **/
+	void gameOver();
+}
